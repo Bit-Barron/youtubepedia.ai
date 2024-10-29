27026faac4e70d94144b078c064f96fcd639f869
Bit-Barron <bit.barron22@gmail.com>
2024-10-29 21:46:45 +0100

    clean up

--- src/lib/i18n/index.ts
+++ src/lib/i18n/index.ts
@@ -4,8 +4,8 @@ import { init, register } from 'svelte-i18n';
 
 const defaultLocale = 'en';
 
-register('en', () => import('../../locales/en.json'));
-register('de', () => import('../../locales/de.json'));
+register('en', () => import('../locales/en.json'));
+register('de', () => import('../locales/de.json'));
 
 init({
 	fallbackLocale: defaultLocale,
