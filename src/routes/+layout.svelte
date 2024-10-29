f3b8231b6c92a23608af80cf80eb7497f0d6c575
Bit-Barron <bit.barron22@gmail.com>
2024-10-29 02:13:50 +0100

    clean up

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script>
 	import '../app.css';
 	import { ModeWatcher } from 'mode-watcher';
+	import { Toaster } from '$lib/components/ui/sonner';
 </script>
 
 <ModeWatcher />
 
+<Toaster />
 <slot></slot>
