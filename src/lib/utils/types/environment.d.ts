133bf450921b61be37a40579bb4a95f1fc6e56bc
Bit-Barron <bit.barron22@gmail.com>
2024-10-29 21:53:36 +0100

    clean up

--- src/lib/utils/types/environment.d.ts
+++ src/lib/utils/types/environment.d.ts
@@ -1,9 +1,13 @@
 declare namespace NodeJS {
 	export interface ProcessEnv {
-		GOOGLE_CLIENT_ID: string;
-		GOOGLE_CLIENT_SECRET: string;
+		DATABASE_URL: string;
+		SMTP_HOST: string;
+		SMTP_PORT: string;
+		SMTP_SERVICE: string;
 		SMTP_USER: string;
 		SMTP_PASS: string;
+		GOOGLE_CLIENT_ID: string;
+		GOOGLE_CLIENT_SECRET: string;
 		PUBLIC_SITE_URL: string;
 		GROQ_API_KEY: string;
 	}
