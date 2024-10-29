1e7e3437e81287d131266aab817bf817eb379c47
Bit-Barron <bit.barron22@gmail.com>
2024-10-29 01:19:16 +0100

    clean up

--- src/lib/components/container/navbar/navbar.svelte
+++ src/lib/components/container/navbar/navbar.svelte
@@ -1,8 +1,19 @@
 <script lang="ts">
 	import { Home, DollarSign, Mail, LogIn, LogOut } from 'lucide-svelte';
-	import { goto } from '$app/navigation';
+	import { goto, invalidateAll } from '$app/navigation';
 
 	export let data;
+
+	async function handleLogout() {
+		const response = await fetch('/api/logout', {
+			method: 'POST'
+		});
+
+		if (response.ok) {
+			await invalidateAll();
+			goto('/');
+		}
+	}
 </script>
 
 <header class="flex items-center justify-between p-4">
@@ -21,14 +32,13 @@
 	{#if data.user}
 		<div class="flex items-center space-x-4">
 			<span class="text-white">{data.user.email}</span>
-			<form action="/logout" method="POST">
-				<button
-					type="submit"
-					class="flex items-center rounded-full bg-red-500 px-4 py-2 text-white transition-colors duration-200 hover:bg-red-600"
-				>
-					<LogOut class="mr-2 h-4 w-4" /> Log Out
-				</button>
-			</form>
+			<button
+				on:click={handleLogout}
+				type="submit"
+				class="flex items-center rounded-full bg-red-500 px-4 py-2 text-white transition-colors duration-200 hover:bg-red-600"
+			>
+				<LogOut class="mr-2 h-4 w-4" /> Log Out
+			</button>
 		</div>
 	{:else}
 		<button
