918e72cf2f9b8c52757dc3602936a9795a9b997d
Bit-Barron <bit.barron22@gmail.com>
2024-10-31 08:38:52 +0100

    asd

--- src/lib/utils/types/index.ts
+++ src/lib/utils/types/index.ts
@@ -8,3 +8,16 @@ export interface ChatData {
 	transcript: string;
 	messages: Message[];
 }
+
+import type { Server as SocketIOServer } from 'socket.io';
+
+declare global {
+	// eslint-disable-next-line no-var
+	var __socketServer: SocketIOServer | undefined;
+
+	interface Global {
+		__socketServer: SocketIOServer | undefined;
+	}
+}
+
+export {};
